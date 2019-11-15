3193c454db0a5116abba271fbcba252cbba96d4d
mike <michgoldennathan17@gmail.com>
2019-11-15 10:58:44 +0100

    [Feat] added main.rb file,gemfile,rubocop.yml & stickler files

---
+++ main.rb
@@ -0,0 +1,51 @@
+# rubocop:disable Style/CaseEquality
+
+module Enumerable
+  def my_each
+
+  end
+
+  def my_each_with_index
+    
+  end
+
+  def my_select
+    
+  end
+
+  def my_all?
+    
+  end
+
+  def my_any?
+    
+  end
+
+  def my_none?
+    
+  end
+
+  def my_count
+    
+  end
+
+  def my_map
+    
+  end
+
+  def my_inject
+    
+  end
+
+  def multiply_els
+    
+  end
+
+  def my_map_with_proc
+    
+  end
+
+  def my_map_proc_or_block
+    
+  end
+end
