26be5a9bee38268aebe34a86d0c6ba99e3e3eb07
mike <michgoldennathan17@gmail.com>
2019-11-15 17:06:05 +0100

    [Feat] added my_each and my_each with index methods to main.rb file

--- main.rb
+++ main.rb
@@ -1,51 +1,47 @@
-# rubocop:disable Style/CaseEquality
+# frozen_string_literal: true
 
 module Enumerable
   def my_each
+    return to_enum unless block_given?
 
+    entry = is_a?(Range) ? to_a : self
+    i = 0
+    while i < size
+      yield(entry[i])
+      i += 1
+    end
+    entry
   end
 
   def my_each_with_index
-    
+    return to_enum unless block_given?
+
+    entry = is_a(Range) ? to_a : self
+    i = 0
+    while i < size
+      yield(entry[i])
+      i += 1
+    end
+    entry
   end
 
-  def my_select
-    
-  end
+  def my_select; end
 
-  def my_all?
-    
-  end
+  def my_all?; end
 
-  def my_any?
-    
-  end
+  def my_any?; end
 
-  def my_none?
-    
-  end
+  def my_none?; end
 
-  def my_count
-    
-  end
+  def my_count; end
 
-  def my_map
-    
-  end
+  def my_map; end
 
-  def my_inject
-    
-  end
+  def my_inject; end
 
-  def multiply_els
-    
-  end
+  def multiply_els; end
 
-  def my_map_with_proc
-    
-  end
+  def my_map_with_proc; end
 
-  def my_map_proc_or_block
-    
-  end
+  def my_map_proc_or_block; end
 end
