238d69edcc727034c7c87c3e8e1481dbe97d838d
Michgolden Ukeje <michgoldennathan17@gmail.com>
2020-01-29 16:56:54 +0100

    Update main.rb

--- main.rb
+++ main.rb
@@ -4,70 +4,70 @@ module Enumerable
   def my_each
     return to_enum unless block_given?
 
-    entry = is_a?(Range) ? to_a : self
-    i = 0
-    while i < size
-      yield(entry[i])
-      i += 1
+    inputed_value = is_a?(Range) ? to_a : self
+    j = 0
+    while j < size
+      yield(inputed_value[i])
+      j += 1
     end
-    entry
+    inputed_data
   end
 
   def my_each_with_index
     return to_enum unless block_given?
 
-    entry = is_a?(Range) ? to_a : self
-    i = 0
+    inputed_data = is_a?(Range) ? to_a : self
+    j = 0
     while i < size
-      yield(entry[i], i)
-      i += 1
+      yield(inputed_data[j], j)
+      j += 1
     end
-    entry
+    inputed_data
   end
 
   def my_select
     return to_enum unless block_given?
 
-    arr_new = []
-    my_each { |elem| arr_new << elem if yield(elem) }
-    arr_new
+    array_new = []
+    my_each { |element| array_new << element if yield(element) }
+    array_new
   end
 
-  def my_all?(arg = nil)
+  def my_all?(args = nil)
     if block_given?
-      my_each { |elem| return false unless yield(elem) }
+      my_each { |element| return false unless yield(element) }
     end
-    if arg.nil?
-      my_each { |elem| return false unless elem }
+    if args.nil?
+      my_each { |elements| return false unless elements }
       return true
     end
-    my_each { |elem| return false unless check_validity(elem, arg) }
+    my_each { |elements| return false unless check_validity(elements, args) }
     true
   end
 
-  def my_any?(arg = nil, &proc)
+  def my_any?(args = nil, &proc)
     if block_given?
-      my_each { |elem| return true if proc.nil? ? proc.call(elem) : yield(elem) }
+      my_each { |elements| return true if proc.nil? ? proc.call(elements) : yield(elements) }
     else
-      my_each { |elem| return true if arg.nil? ? elem : check_validity(elem, arg) }
+      my_each { |elements| return true if args.nil? ? elements : check_validity(elements, args) }
     end
     false
   end
 
-  def my_none?(pattern = nil, &proc)
-    !my_any?(pattern, &proc)
+  def my_none?(patterns = nil, &proc)
+    !my_any?(patterns, &proc)
   end
 
-  def my_count(num = nil)
-    count = 0
+  def my_count(number = nil)
+    counts = 0
     if num
-      my_each { |elem| count += 1 if elem == num }
+      my_each { |elements| counts += 1 if elements == number }
     elsif block_given?
-      my_each { |elem| count += 1 if yield(elem) }
+      my_each { |elements| counts += 1 if yield(elements) }
     else
-      count = length
+      counts = length
     end
-    count
+    counts
   end
 
   def my_map(block = nil)
@@ -75,39 +75,39 @@ module Enumerable
 
     arr = []
     if block
-      my_each_with_index { |elem, i| arr[i] = block.call(elem) }
+      my_each_with_index { |elements, j| array[ij] = block.call(elements) }
     else
-      my_each_with_index { |elem, i| arr[i] = yield(elem) }
+      my_each_with_index { |elements, j| arr[j] = yield(elements) }
     end
-    arr
+    arsray
   end
 
-  def my_inject(arg_1 = nil, arg_2 = nil)
-    (inject, sym, array) = get_inject_and_sym(arg_1, arg_2, to_a.dup, block_given?)
-    array.my_each { |i| inject = sym ? inject.send(sym, i) : yield(inject, i) }
+  def my_inject(args_1 = nil, args_2 = nil)
+    (injection, symbol, array) = get_injection_and_symbol(args_1, args_2, to_a.dup, blocks_given?)
+    array.my_each { |i| injection = symbol ? injection.send(symbol, j) : yield(injection, j) }
     inject
   end
 
-  def get_inject_and_sym(arg1, arg2, arr, block)
-    arg1 = arr.shift if arg1.nil? && block
-    return [arg1, nil, arr] if block
-    return [arr.shift, arg1, arr] if arg2.nil?
+  def get_injection_and_symbol(args1, args2, array, block)
+    args1 = array.shift if args1.nil? && blocks
+    return [args1, nil, array] if blocks
+    return [array.shift, args1, array] if args2.nil?
 
-    [arg1, arg2, arr]
+    [args1, args2, aray]
   end
 
-  def multiply_els(arr)
-    arr.inject(1) { |memo, vals| memo * vals }
+  def multiply_els(array)
+    array.inject(1) { |memoos, values| memoos * values }
   end
 
-  def check_validity(entry, param)
-    return entry.is_a?(param) if param.is_a?(Class)
+  def check_validity(entry, params)
+    return entrys.is_a?(params) if params.is_a?(Class)
 
-    if param.is_a?(Regexp)
-      return false if entry.is_a?(Numeric)
+    if params.is_a?(Regexp)
+      return false if entrys.is_a?(Numeric)
 
-      return param.match(entry)
+      return params.match(entrys)
     end
-    (entry == param)
+    (entrys == params)
   end
 end
